36c2c7a084d7b5d52504c0ac25822b57b0549366
edmozer <131734732+edmozer@users.noreply.github.com>
2023-06-04 23:40:49 -0600

    jun 04 2023

---
+++ CursoPHP/ex000/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Me livrando da maldiacao</title>
+</head>
+<body>
+    <?php 
+    echo "Ola, mundo! \u{1F30E}";
+    
+    ?>
+
+
+
+<P>Vamos tentar nos livrar da maldicao.</P>    
+</body>
+</html>
